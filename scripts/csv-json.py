cae9791bd73bc9e7ab9ce626a02d27a84cbeb86f
Yohan Avishke <yaediriweera@gmail.com>
2021-02-19 19:03:30 +0530

    Add csv to josn script

--- scripts/csv-json.py
+++ scripts/csv-json.py
@@ -1,11 +1,27 @@
 import csv
 import json
 
-csvfile = open('file.csv', 'r')
-jsonfile = open('file.json', 'w')
+csvfile = open('../data/inventory/price_list_10-02-2021..csv', 'r')
+jsonfile = open('../data/inventory/price_list_10-02-2021.json', 'w')
 
-fieldnames = ("FirstName","LastName","IDNumber","Message")
+fieldnames = ('Part Code','Description','PR Code','Price')
 reader = csv.DictReader( csvfile, fieldnames)
-for row in reader:
-    json.dump(row, jsonfile)
-    jsonfile.write('\n')
+
+jsonfile.write('[')
+for idx,row in enumerate(reader):
+	if idx != 0:
+		desc = row['Description']
+		price = row['Price']
+
+		desc = desc.replace('\n', '')
+		if desc[-1] == ' ':
+			desc = desc[:-1]	
+		row['Description'] = desc
+
+		price = int(price.replace(',',''))
+		row['Price'] = price
+
+		json.dump(row, jsonfile)
+		jsonfile.write(',\n')
+
+jsonfile.write(']')
