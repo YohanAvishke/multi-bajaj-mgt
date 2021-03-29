5d0db5208190adc27a0754971dcfdde3a79373f9
Yohan Avishke <yaediriweera@gmail.com>
2021-03-29 20:11:39 +0530

    Add functions to find duplicates

--- scripts/product.py
+++ scripts/product.py
@@ -4,14 +4,10 @@ import csv
 filePath = '/Users/yohanavishke/Documents/Projects/YohanAvishke/multi-bajaj-pos/data/'
 
 
-def get_template():
-    with open(f'{filePath}templates/products.csv') as csv_file:
-        csv_template = csv.reader(csv_file, delimiter=',')
-        for row in csv_template:
-            return row
-
-
 def convert_to_json():
+    """
+    Convert product_prices to json from csv
+    """
     csv_file = open(f'{filePath}data files/product/product-prices.csv', 'r')
     json_file = open(f'{filePath}data files/product/product-prices.json', 'w')
 
@@ -21,96 +17,55 @@ def convert_to_json():
     json_file.write('[')
     for idx, row in enumerate(reader):
         if idx != 0:
-            desc = row['PART_DESC']
-            price = row['SELLING PRICE']
-
-            newline_pieces = desc.split('\n')
-            newline_pieces_size = len(newline_pieces)
-            if newline_pieces_size == 2:
-                if newline_pieces[0] != '':
-                    desc = newline_pieces[0]
-                elif newline_pieces[1] != '':
-                    desc = newline_pieces[1]
-                else:
-                    print(f'{row["PART_NO"]} - ALERT: PIECE SIZE: 2, BOTH ARE EMPTY')
-                    exit(2)
-            elif newline_pieces_size == 3:
-                if newline_pieces[0] != '':
-                    desc = newline_pieces[0]
-                elif newline_pieces[1] != '':
-                    desc = newline_pieces[1]
-                elif newline_pieces[2] != '':
-                    desc = newline_pieces[2]
-                else:
-                    print(f'{row["PART_NO"]} - ALERT: PIECE SIZE: 3, ALL ARE EMPTY')
-                    exit(2)
-            elif newline_pieces_size == 4:
-                if newline_pieces[0] != '':
-                    desc = newline_pieces[0]
-                elif newline_pieces[1] != '':
-                    desc = newline_pieces[1]
-                elif newline_pieces[2] != '':
-                    desc = newline_pieces[2]
-                elif newline_pieces[3] != '':
-                    desc = newline_pieces[3]
-                else:
-                    print(f'{row["PART_NO"]} - ALERT: PIECE SIZE: 4, ALL ARE EMPTY')
-                    exit(2)
-            elif newline_pieces_size > 4:
-                print(f'{row["PART_NO"]} - ALERT: PIECE SIZE: {newline_pieces_size}')
-                exit(2)
-
-            desc = desc.replace('\n', '').replace('\r', '')
-            row['PART_DESC'] = desc.replace('\u00a0', ' ').replace('\u201c', '').replace('\u201d', '') \
-                .replace('\u00bf', '-').replace('\u00d8', '0')
-
-            price = int(price.replace(',', ''))
-            row['SELLING PRICE'] = price
-
             json.dump(row, json_file)
             json_file.write(',\n')
     json_file.write(']')
 
 
-def test():
-    with open(f'{filePath}data files/product/product-categories') as f:
+def reformat_data():
+    """
+    Update product_prices from product_categories' description and price as those can contain non-formatted strings
+    """
+    with open(f'{filePath}data files/product/product-categories.json') as f:
         product_categories = json.load(f)
     with open(f'{filePath}data files/product/product-prices.json') as f:
         product_prices = json.load(f)
 
+    for product_price in product_prices:
+        product_price['SELLING PRICE'] = float(product_price['SELLING PRICE'].replace(',', ''))
 
-def enrich_products():
-    enriched_products = []
+        for product_category in product_categories:
+            if product_price['PART_NO'] == product_category['Part Code']:
+                desc = product_category['Product Description'].replace('\n', '').replace('\r', '').replace('\t', '')
+                product_price['PART_DESC'] = desc
+                print(product_price)
+                break
 
-    with open(f'{filePath}data files/product/product-categories') as f:
-        product_categories = json.load(f)
-    with open(f'{filePath}data files/product/product-prices.csv') as f:
-        product_prices = csv.DictReader(f)
-
-    # for product_price in product_prices:
-    #     for product_category in product_categories:
-    #         if product_category['Part Code'] == product_pr['Part Code'] and product_category['Pro Hier Code'] == 'BAJ':
-    #             description = product_category['Description'].replace('\n', '').replace('\r', '')
-    #             # price = int(product_pr['Price'].replace(',', ''))
-    #
-    #             enriched_product = {'Part Code': product_category['Part Code'],
-    #                                 'Description': description,
-    #                                 'Price': product_pr['Price'],
-    #                                 'PR Code': product_pr['PR Code'],
-    #                                 'Serial Base': product_category['Serial Base'],
-    #                                 'Pro Hier Code': product_category['Pro Hier Code'],
-    #                                 'MOQ Value': product_category['MOQ Value'],
-    #                                 'Category': f'point_of_sale.{product_category["Part cat"]}'}
-    #             enriched_products.append(enriched_product)
-    #             # print(enriched_product)
-    #             found = True
-    #             break
-
-    # with open(f'{filePath}data files/product/products.json', 'w') as f:
-    #     json.dump(enriched_products, f)
+    json_file = open(f'{filePath}data files/product/product-prices.json', 'w')
+    json.dump(product_prices, json_file)
 
 
-def generate_csv():
+def add_categories():
+    """
+    Add categories to the products and write them to a different json file
+    """
+    with open(f'{filePath}data files/product/product-details.json') as f:
+        product_details = json.load(f)
+    with open(f'{filePath}data files/product/product-prices.json') as f:
+        product_prices = json.load(f)
+
+    for product_price in product_prices:
+        for product_detail in product_details:
+            if product_price['PART_NO'] == product_detail['Part Code']:
+                product_price['CATEGORY'] = f'point_of_sale.{product_detail["Part cat"]}'
+                print(product_price)
+                break
+
+    with open(f'{filePath}data files/product/products.json', 'w') as f:
+        json.dump(product_prices, f)
+
+
+def convert_to_csv():
     with open(f'{filePath}data files/product/products.json') as f:
         json_products = json.load(f)
 
@@ -120,16 +75,59 @@ def generate_csv():
 
         for json_product in json_products:
             csv_writer.writerow(
-                [f'point_of_sale.{json_product["Part Code"]}', f'{json_product["Part Code"]}',
-                 f'{json_product["Description"]}', f'{json_product["Part Code"]}',
-                 f'{json_product["Description"]}', f'{json_product["Category"]}',
-                 'Storable Product', f'{json_product["Price"]}', f'{json_product["Price"]}',
+                [f'point_of_sale.{json_product["PART_NO"]}', f'{json_product["PART_NO"]}',
+                 f'{json_product["PART_DESC"]}', f'{json_product["PART_NO"]}',
+                 f'{json_product["PART_DESC"]}', f'{json_product["CATEGORY"]}',
+                 'Storable Product', f'{json_product["SELLING PRICE"]}', f'{json_product["SELLING PRICE"]}',
                  True, True, True
                  ])
             print(json_product)
 
 
-test()
+def get_template():
+    with open(f'{filePath}templates/products.csv') as csv_file:
+        csv_template = csv.reader(csv_file, delimiter=',')
+        for row in csv_template:
+            return row
+
+
+def validate_data():
+    """
+    Detect duplicates in the final csv file
+    """
+    products = open(f'{filePath}data files/product/products.csv', 'r')
+    existing_products = open(f'{filePath}data files/product/product.template.csv', 'r')
+    column_names = ('External ID', 'Internal Reference', 'Name', 'Barcode', 'Description',
+                    'Product Category/External ID', 'Product Type', 'Sales Price', 'Available in POS',
+                    'Can be Purchased', 'Can be Sold')
+    products_r = list(csv.DictReader(products, column_names))
+    column_names = ('Internal Reference', 'Display Name')
+    existing_products_r = list(csv.DictReader(existing_products, column_names))
+
+    count = 0
+    for product_r1 in products_r:
+        flag = False
+        for product_r2 in products_r:
+            if product_r1['Internal Reference'] == product_r2['Internal Reference']:
+                if flag:
+                    print(f'Duplicate {product_r1["Internal Reference"]}')
+                flag = True
+                count += 1
+        # if flag:
+        #     print(count)
+    # for product_r in products_r:
+    #     flag = False
+    #     for existing_product_r in existing_products_r:
+    #         if product_r['Internal Reference'] == existing_product_r['Internal Reference']:
+    #             flag = True
+    #             break
+    #     if not flag:
+    #         print(product_r['Internal Reference'])
+
 # convert_to_json()
-# enrich_products()
-# generate_csv()
+# TODO: remove last comma from last object for next cmd to work in product_prices.json(Only if convert_to_json() has
+#  been called before)
+# reformat_data()
+# add_categories()
+# convert_to_csv()
+# validate_data()
