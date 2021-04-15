91476bc23a51e2790ea0487c4db2364c74d4b647
Yohan Avishke <yaediriweera@gmail.com>
2021-04-15 19:03:02 +0530

    Update script with error handling

--- scripts/price-reader.py
+++ scripts/price-reader.py
@@ -1,31 +1,46 @@
 import requests
 import json
+import time
 
 url = "https://erp.dpg.lk/PADEALER/PADLRItemInquiry/Inquire"
 
 headers = {
-  'authority': 'erp.dpg.lk',
-  'sec-ch-ua': '"Google Chrome";v="89", "Chromium";v="89", ";Not A Brand";v="99"',
-  'accept': '*/*',
-  'x-requested-with': 'XMLHttpRequest',
-  'sec-ch-ua-mobile': '?0',
-  'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36',
-  'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-  'origin': 'https://erp.dpg.lk',
-  'sec-fetch-site': 'same-origin',
-  'sec-fetch-mode': 'cors',
-  'sec-fetch-dest': 'empty',
-  'referer': 'https://erp.dpg.lk/Application/Home/PADEALER',
-  'accept-language': 'en-US,en;q=0.9',
-  'cookie': '.AspNetCore.Session=CfDJ8GOndye%2Bf05JhuhX8EajvZbH94vjbEK5H8ZCwsuxHh68%2FYwV2%2FKi0dA%2FTmz3dpt6VzyaBNMAoElmRT%2BbnkqHMMPgyXYkG%2BhzTi0OSIeGceu9%2FaWZD5ddF%2FHzwLMx6hE04wmLMKSMNJUXh6U8XXB9Sz5qO9YawixjrG6n7R9XKt1V; .AspNetCore.Antiforgery.mEZFPqlrlZ8=CfDJ8GOndye-f05JhuhX8EajvZb8OXIKsLSsbSTWzYD3WNvO3OvX_Xuyl6YJ46d5VqtV4JUPfuTAIdPCeSmCK1QIhBC03-G_aOGFkfP4fKjw9FnGeSKESkSAi061F_kVE2IJ_xWtLiAXFV3qIJ9Std1WEKM'
+    'authority': 'erp.dpg.lk',
+    'sec-ch-ua': '"Google Chrome";v="89", "Chromium";v="89", ";Not A Brand";v="99"',
+    'accept': '*/*',
+    'x-requested-with': 'XMLHttpRequest',
+    'sec-ch-ua-mobile': '?0',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+                  '89.0.4389.114 Safari/537.36',
+    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'origin': 'https://erp.dpg.lk',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-dest': 'empty',
+    'referer': 'https://erp.dpg.lk/Application/Home/PADEALER',
+    'accept-language': 'en-US,en;q=0.9',
+    'cookie': '.AspNetCore.Session=CfDJ8H8TYQJVgJRGttyTw7gWFEZoDWDrYJsPTYLKqZdcoYdyYhG3ggsoMRkYnL7V1g5pLuXUNpLdOeaUW%2B'
+              'LAIHd0Mrvle4KtcFHJ0nGqyZZnOCzkUmX6obnOebX30RNKAj%2FkJH%2BMQ8%2FRZKmkxhoPrFIG9pB%2FcpmD5jNd8qRZMe4IsxYh; '
+              '.AspNetCore.Antiforgery.mEZFPqlrlZ8=CfDJ8H8TYQJVgJRGttyTw7gWFEawVVW-1-ZcXNH31kg-kamO9B4yfGVKSI5EoxMByzV1'
+              'CoJk8oA0Qp0mCkNsTqWsvRJtpTyagST5kjTPd5icsGtdPabpCxdvOnAGunsuP6OON-nDcPMTAuqmmNg8NfNS0k0'
 }
 
 numbers = [
 ]
 
 for number in numbers:
-	payload=f'strPartNo_PAItemInq={number}&strFuncType=INVENTORYDATA&strPADealerCode_PAItemInq=AC2011063676&STR_FORM_ID=00602&STR_FUNCTION_ID=IQ&STR_PREMIS=KGL&STR_INSTANT=DLR&STR_APP_ID=00011'
-	
-	response = requests.request("POST", url, headers=headers, data=payload)
+    payload = f'strPartNo_PAItemInq={number}&strFuncType=INVENTORYDATA&strPADealerCode_PAItemInq=AC2011063676&STR_' \
+              f'FORM_ID=00602&STR_FUNCTION_ID=IQ&STR_PREMIS=KGL&STR_INSTANT=DLR&STR_APP_ID=00011'
 
-	print(json.loads(response.text)['DATA']['dblSellingPrice'])
+    response = requests.request("POST", url, headers=headers, data=payload)
+    if response.status_code == 200:
+        data = json.loads(response.text)['DATA']
+
+        if 'dblSellingPrice' in data:
+            print(data['dblSellingPrice'])
+        else:
+            print(f'{number} - Not Found.')
+    else:
+        print(f'{number} - Not Found.')
+
+    time.sleep(5)
