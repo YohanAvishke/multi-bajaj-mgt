4e164597314a8bb837eec21eba404df2366a9bc3
Yohan Avishke <yaediriweera@gmail.com>
2021-04-18 17:40:36 +0530

    Update

--- app/Utils.py
+++ app/Utils.py
@@ -1,21 +1,41 @@
 import csv
 import json
+from collections import OrderedDict
 
 
 def csv_to_json(file_path=None, field_names=None, data_obj=None):
     """
     data_obj  should be [OrderedDict(), ...]
     """
-    catalogue = []
+    objects = []
 
     if file_path is not None:
-        catalogue_file = open(file_path, 'r')
-        data_obj = csv.DictReader(catalogue_file, field_names)
+        file = open(file_path, 'r')
+        data_obj = csv.DictReader(file, field_names)
 
     for idx, row in enumerate(data_obj):
         if idx != 0:
-            catalogue.append(row)
-    catalogue = json.dumps(catalogue, indent=4)
-    catalogue = json.loads(catalogue)
+            objects.append(row)
+    objects = json.dumps(objects, indent=4)
+    objects = json.loads(objects)
 
-    return catalogue
+    return objects
+
+
+def find_duplicates(first_file, first_header, second_file, second_header):
+    """
+    Detect duplicates in the final csv files
+    """
+    first_reader = open(first_file, 'r')
+    second_reader = open(second_file, 'r')
+
+    first_list = list(csv.DictReader(first_reader, first_header))
+    second_list = list(csv.DictReader(second_reader, second_header))
+
+    for product_row_one in first_list:
+        flag = False
+        for product_row_two in second_list:
+            if product_row_one['Internal Reference'] == product_row_two['Internal Reference']:
+                if flag:
+                    print(f'Duplicate {product_row_one["Internal Reference"]}')
+                flag = True
