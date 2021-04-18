bb4760cf65c265c8f3862b2f3c5ff967c52d2915
Yohan Avishke <yaediriweera@gmail.com>
2021-04-18 11:40:23 +0530

    Reformat

--- app/product/Catalogue.py
+++ app/product/Catalogue.py
@@ -1,14 +1,18 @@
 from app.Utils import *
 
+FILE_PATH = '../../data/product/catalogue.csv'
+FIELD_NAMES = ('Part Number', 'Quantity', 'Unit Price', 'Notes')
+
 
 def format_data():
-    catalogue = csv_to_json(FILE_PATH, FIELD_NAMES)
     finalised_products = []
     unidentified_products = {'NOT FOUND': [], 'NO DATA': [], 'ZERO QUANTITY': []}
     third_party_products = {
         'YELLOW LABEL': [], 'SA': [], 'PAL': [], 'MILEX': [], 'MINDA': [], 'SD': [], 'BOSCH': [], 'MB': [], 'NACHi': [],
         'JCM': [], 'NCL': [], 'GLUE': [], 'ORIGINAL': [], 'LOCAL': [], 'UNKNOWN': []
     }
+
+    catalogue = csv_to_json(FILE_PATH, FIELD_NAMES)
 
     for idx, product in enumerate(catalogue):
         note = product['Notes']
@@ -25,12 +29,7 @@ def format_data():
         'Unidentified': unidentified_products,
         'Third Party': third_party_products
     }
-
     print(catalogue)
-
-
-FILE_PATH = '../../data/product/catalogue.csv'
-FIELD_NAMES = ('Part Number', 'Quantity', 'Unit Price', 'Notes')
 
 
 format_data()
