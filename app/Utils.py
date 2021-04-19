41a2d3442ebcbb61331bd87f3b5bd04182633c42
Yohan Avishke <yaediriweera@gmail.com>
2021-04-19 16:13:52 +0530

    Remove not needed data files

--- app/Utils.py
+++ app/Utils.py
@@ -21,21 +21,3 @@ def csv_to_json(file_path=None, field_names=None, data_obj=None):
 
     return objects
 
-
-def find_duplicates(first_file, first_header, second_file, second_header):
-    """
-    Detect duplicates in the final csv files
-    """
-    first_reader = open(first_file, 'r')
-    second_reader = open(second_file, 'r')
-
-    first_list = list(csv.DictReader(first_reader, first_header))
-    second_list = list(csv.DictReader(second_reader, second_header))
-
-    for product_row_one in first_list:
-        flag = False
-        for product_row_two in second_list:
-            if product_row_one['Internal Reference'] == product_row_two['Internal Reference']:
-                if flag:
-                    print(f'Duplicate {product_row_one["Internal Reference"]}')
-                flag = True
--- app/product/Catalogue.py
+++ app/product/Catalogue.py
@@ -9,7 +9,8 @@ def format_data():
     unidentified_products = {'NOT FOUND': [], 'NO DATA': [], 'ZERO QUANTITY': []}
     third_party_products = {
         'YELLOW LABEL': [], 'SA': [], 'PAL': [], 'MILEX': [], 'MINDA': [], 'SD': [], 'BOSCH': [], 'MB': [], 'NACHi': [],
-        'JCM': [], 'NCL': [], 'GLUE': [], 'ORIGINAL': [], 'LOCAL': [], 'UNKNOWN': []
+        'JCM': [], 'NCL': [], 'Lumax': [], 'MacRo': [], 'Varroc': [], 'Flash': [], 'Champion': [], 'GLUE': [],
+        'ORIGINAL': [], 'LOCAL': [], 'UNKNOWN': []
     }
 
     catalogue = csv_to_json(FILE_PATH, FIELD_NAMES)
@@ -18,8 +19,8 @@ def format_data():
         note = product['Notes']
         if note in ('NOT FOUND', 'NO DATA', 'ZERO QUANTITY'):
             unidentified_products[note].append(product)
-        elif note in ('YELLOW LABEL', 'SA', 'PAL', 'MILEX', 'MINDA', 'SD', 'BOSCH', 'MB', 'NACHi', 'JCM', 'NCL', 'GLUE',
-                      'ORIGINAL', 'LOCAL', 'UNKNOWN'):
+        elif note in ('YELLOW LABEL', 'SA', 'PAL', 'MILEX', 'MINDA', 'SD', 'BOSCH', 'MB', 'NACHi', 'JCM', 'NCL',
+                      'Lumax', 'MacRo', 'Varroc', 'Flash', 'Champion', 'GLUE', 'ORIGINAL', 'LOCAL', 'UNKNOWN'):
             product['Part Number'] = product['Part Number'].split("Y-")[-1]
             third_party_products[note].append(product)
         else:
@@ -39,7 +40,7 @@ def format_data():
         if catalogue_type == 'Finalised':
             for product in catalogues:
                 csv_writer.writerow([
-                    product['Part Number'] , product['Quantity'], product['Unit Price'], product['Notes']
+                    product['Part Number'], product['Quantity'], product['Unit Price'], product['Notes']
                 ])
         else:
             for note in catalogues:
@@ -51,33 +52,75 @@ def format_data():
     data_file.close()
 
 
+def missing_products():
+    """
+    Detect duplicates in the final csv files
+    """
+    shop_reader = open('../../data/product/catalogue/shop-catalogue(sorted).csv', 'r')
+    odoo_reader = open('../../data/product/catalogue/odoo-catalogue.csv', 'r')
+
+    headers = ('Part Number', 'Quantity', 'Unit Price', 'Notes')
+    shop_catalogue = list(csv.DictReader(shop_reader, headers))
+    headers = ('Internal Reference', 'Display Name')
+    odoo_catalogue = list(csv.DictReader(odoo_reader, headers))
+
+    for shop_product in shop_catalogue:
+        is_found = False
+        part_number = shop_product['Part Number']
+        if shop_product['Notes'] == '':
+            for odoo_product in odoo_catalogue:
+                if part_number == odoo_product['Internal Reference']:
+                    is_found = True
+            if not is_found:
+                print(shop_product)
 
 
-    # objects.append(OrderedDict([
-    #     ("Part Number", "Part Number"), ("Quantity", "Quantity"), ("Unit Price", "Unit Price"), ("Notes", "Notes")
-    # ]))
-    # for key, value in enumerate(data_obj):
-    #     print(f'{key} - {value}')
-    # if row == 'Third Party':
-    # for key, value in data_obj[row]:
-    #     print(f'{key} - {value}')
-    # if idx == 0:
-    #     objects.append(OrderedDict([
-    #         ("type", "Third Party"), ("Part Number", product["Part Number"]), ("Quantity", product["Quantity"]),
-    #         ("Unit Price", product["Unit Price"]), ("Notes", product["Notes"])
-    #     ]))
-    # else:
-    #     objects.append(OrderedDict([
-    #         ("type", ""), ("Part Number", product["Part Number"]), ("Quantity", product["Quantity"]),
-    #         ("Unit Price", product["Unit Price"]), ("Notes", product["Notes"])
-    #     ]))
-    #     elif row == 'Unidentified':
-    #         print(type(data_obj[row]))
-    #     else:
-    #         print(type(data_obj[row]))
-    # print(objects)
+def update_prices():
+    shop_reader = open('../../data/product/catalogue/shop-catalogue(sorted).csv', 'r')
+    odoo_reader = open('../../data/product.template.csv', 'r')
+    odoo_write = open('../../data/product/catalogue/odoo-catalogue(priced).csv', 'w')
 
-    # json_to_csv(data_obj=catalogue)
+    headers = ('Part Number', 'Quantity', 'Unit Price', 'Notes')
+    shop_catalogue = list(csv.DictReader(shop_reader, headers))
+    headers = ('External ID', 'Internal Reference', 'Sales Price', 'Cost')
+    odoo_catalogue = list(csv.DictReader(odoo_reader, headers))
+
+    for idx, shop_product in enumerate(shop_catalogue):
+        part_number = shop_product['Part Number']
+        if shop_product['Notes'] == '':
+            for odoo_product in odoo_catalogue:
+                if part_number == odoo_product['Internal Reference']:
+                    odoo_product['Sales Price'] = shop_product['Unit Price']
+                    odoo_product['Cost'] = shop_product['Unit Price']
+                    # print(idx)
+
+    # print(odoo_catalogue)
+    with open(f'../../data/odoo-catalogue(priced).json', 'w') as f:
+        json.dump(odoo_catalogue, f)
+
+    # with open(f'../../data/product/catalogue/odoo-catalogue(priced).csv', mode='w') as csvFile:
+    #     csv_writer = csv.writer(csvFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    #     for odoo_product in odoo_catalogue:
+    #         csv_writer.writerow(odoo_product)
 
 
-format_data()
+def convert_to_csv():
+    with open(f'../../data/odoo-catalogue(priced).json') as f:
+        json_products = json.load(f)
+
+    with open(f'../../data/odoo-catalogue(priced).csv', mode='w') as csvFile:
+        csv_writer = csv.writer(csvFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        # csv_writer.writerow(get_template())
+
+        for json_product in json_products:
+            csv_writer.writerow(
+                [f'{json_product["External ID"]}', f'{json_product["Internal Reference"]}', f'{json_product["Sales Price"]}',
+                 f'{json_product["Cost"]}'
+                 ])
+            print(json_product)
+
+
+# update_prices()
+convert_to_csv()
+# format_data()
+# missing_products()
