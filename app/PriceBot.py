1ff2790f63fb3bd9fbd1cd89178ec2b83ae1da8c
Yohan Avishke <yaediriweera@gmail.com>
2021-05-11 20:21:04 +0530

    Update log format

--- app/PriceBot.py
+++ app/PriceBot.py
@@ -35,7 +35,7 @@ HEADERS = {
 
 # -*- Main function -*-
 if __name__ == "__main__":
-    logging_format = "%(asctime)s: %(levelname)s- %(message)s"
+    logging_format = "%(asctime)s: %(levelname)s - %(message)s"
     logging.basicConfig(format=logging_format, level=logging.INFO, datefmt="%H:%M:%S")
 
 
