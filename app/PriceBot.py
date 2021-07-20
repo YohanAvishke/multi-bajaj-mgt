b3298dae3ab5ec92f89ad57dddaf15ff80c79a22
Yohan Avishke <yaediriweera@gmail.com>
2021-07-20 20:07:49 +0530

    Update prices

--- app/PriceBot.py
+++ app/PriceBot.py
@@ -26,12 +26,7 @@ HEADERS = {
     "sec-fetch-dest": "empty",
     "referer": "https://erp.dpg.lk/Application/Home/PADEALER",
     "accept-language": "en-US,en;q=0.9",
-    'cookie': '.AspNetCore.Session=CfDJ8JddFXf%2BYTNGr3%2FmRSRGhG6BrWigXN%2FZgcYBU4e3vNa6G2YA5oHMf'
-              '%2B7eI6bS6fKbvZ6eRmApn0z5bNnJjINdYyKLKN9g124iXUa7SYUzOuR8U1VgiTVBn%2FybiCrazuyW82G3WKhkDRGaG%2FA23hnPT'
-              '%2F4ncWXbhXU8wHAkxwkzvynU; '
-              '.AspNetCore.Antiforgery.mEZFPqlrlZ8=CfDJ8JddFXf'
-              '-YTNGr3_mRSRGhG4YDvnWK_h1prGGRVrHCQVPg9W6CUevo3hU5ARSyRMb-suYh'
-              '-UGb841iCvOaynbU2JrkrwEmOcU3fKLRYvF2Lb48wiBQlRvQDSkCCoZyLytqUSOFFPbrJyLaimYd0ZiITM'
+    'cookie': '.AspNetCore.Session=CfDJ8JddFXf%2BYTNGr3%2FmRSRGhG6BrWigXN%2FZgcYBU4e3vNa6G2YA5oHMf%2B7eI6bS6fKbvZ6eRmApn0z5bNnJjINdYyKLKN9g124iXUa7SYUzOuR8U1VgiTVBn%2FybiCrazuyW82G3WKhkDRGaG%2FA23hnPT%2F4ncWXbhXU8wHAkxwkzvynU; .AspNetCore.Antiforgery.mEZFPqlrlZ8=CfDJ8JddFXf-YTNGr3_mRSRGhG4YDvnWK_h1prGGRVrHCQVPg9W6CUevo3hU5ARSyRMb-suYh-UGb841iCvOaynbU2JrkrwEmOcU3fKLRYvF2Lb48wiBQlRvQDSkCCoZyLytqUSOFFPbrJyLaimYd0ZiITM'
     }
 
 # -*- Main function -*-
