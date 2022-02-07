df7c254530d7cfacd647f6a2e89e03fd188f27e6
Yohan Avishke <yaediriweera@gmail.com>
2022-02-07 23:46:26 +0530

    Create adjustments

--- app/bots/InventoryBot.py
+++ app/bots/InventoryBot.py
@@ -227,7 +227,7 @@ def inventory_adjustment(dated_adj_file):
 
 
 def get_sales_adjustments():
-    # sheet_client.main()
+    sheet_client.main()
     inventory_adjustment(sheet_client.DATED_ADJUSTMENT_FILE)
 
 
@@ -247,4 +247,4 @@ def get_dpmc_adjustments():
 if __name__ == "__main__":
     logging_format = "%(asctime)s: %(levelname)s - %(message)s"
     logging.basicConfig(format = logging_format, level = logging.INFO, datefmt = "%H:%M:%S")
-    get_sales_adjustments()
+    get_dpmc_adjustments()
