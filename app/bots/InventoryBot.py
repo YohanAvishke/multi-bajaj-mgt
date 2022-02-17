519b63d0a17a21a31f7a6ef7f9ccc85fcb6ebf35
Yohan Avishke <yaediriweera@gmail.com>
2022-02-17 05:49:10 +0530

    Create adj

--- app/bots/InventoryBot.py
+++ app/bots/InventoryBot.py
@@ -227,24 +227,24 @@ def inventory_adjustment(dated_adj_file):
 
 
 def get_sales_adjustments():
-    # sheet_client.main()
+    sheet_client.main()
     inventory_adjustment(sheet_client.DATED_ADJUSTMENT_FILE)
 
 
 def get_other_adjustments():
-    # _save_dated_adjustment(ADJ_OTHER_FILE)
+    _save_dated_adjustment(ADJ_OTHER_FILE)
     inventory_adjustment(DATED_ADJUSTMENT_FILE)
 
 
 def get_dpmc_adjustments():
-    dpmc_client.authenticate()
-    _fetch_grn_invoice()
-    _fetch_products()
-    _save_dated_adjustment(ADJ_DPMC_FILE)
-    # inventory_adjustment(DATED_ADJUSTMENT_FILE)
+    # dpmc_client.authenticate()
+    # _fetch_grn_invoice()
+    # _fetch_products()
+    # _save_dated_adjustment(ADJ_DPMC_FILE)
+    inventory_adjustment(DATED_ADJUSTMENT_FILE)
 
 
 if __name__ == "__main__":
     logging_format = "%(asctime)s: %(levelname)s - %(message)s"
     logging.basicConfig(format = logging_format, level = logging.INFO, datefmt = "%H:%M:%S")
-    get_sales_adjustments()
+    get_dpmc_adjustments()
