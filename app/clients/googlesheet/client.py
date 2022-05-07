8c993dc5cbcf14265cefcc694da785ce21f83efd
YohanAvishke <yaediriweera@gmail.com>
2022-05-07 08:32:58 +0530

    Create dpmc adjustment

--- app/clients/googlesheet/client.py
+++ app/clients/googlesheet/client.py
@@ -5,7 +5,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from datetime import date
-from app.config import ROOT_DIR
+from ...config import ROOT_DIR
 
 import json
 import os.path
