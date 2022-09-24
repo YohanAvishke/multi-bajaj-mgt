46d9d2ec829951a63309a16a5e4114d7cee5ffb4
YohanAvishke <yaediriweera@gmail.com>
2022-09-24 19:35:14 +0530

    Complete doc strings

--- src/multibajajmgt/clients/odoo.py
+++ src/multibajajmgt/clients/odoo.py
@@ -7,7 +7,7 @@ from multibajajmgt.config import (
     ODOO_SERVER_URL as SERVER_URL,
     ODOO_SERVER_USERNAME as SERVER_USERNAME,
     ODOO_SERVER_API_KEY as SERVER_API_KEY,
-    ODOO_DATABASE_NAME as DATABASE_NAME,
+    ODOO_DATABASE_NAME as DATABASE_NAME
 )
 
 log = logging.getLogger(__name__)
@@ -15,6 +15,15 @@ user_id = None
 
 
 def _json_rpc(url, method, params):
+    """Create Requests to Odoo's JSON-RPC server
+
+    Common wrapper method for all calls
+
+    :param url: string, url of the endpoint
+    :param method: string
+    :param params: list, params for the payload of the request
+    :return: json dict, response body
+    """
     data = {
         "jsonrpc": "2.0",
         "method": method,
@@ -37,10 +46,22 @@ def _json_rpc(url, method, params):
 
 
 def _call(url, service, method, *args):
+    """Method to setup JSON RPC call's Arguments.
+
+    :param url: string, url of the endpoint
+    :param service: string, final part of the subdirectory(of the url)
+    :param method: string, method to be executed on the request
+    :param args: tuple, args for the payload's params of the request(authentication info, module name, etc.)
+    :return: json dict, response body
+    """
     return _json_rpc(url, "call", {"service": service, "method": method, "args": args})
 
 
 def _authenticate():
+    """ Get User-ID to verify Username and API Key.
+
+    Save the User-ID for future Requests from the Client.
+    """
     global user_id
     if not user_id:
         log.info("Authenticating odoo-client and setting up the user-id")
@@ -48,11 +69,13 @@ def _authenticate():
 
 
 def fetch_product_external_id(db_id_list, limit = 0):
-    """
-    External ids are necessary for importing data
-    :param limit: limit result count
-    :param db_id_list: products database reference id
-    :return: dataframe of dicts
+    """Fetch External IDs for a list of product.template Primary Keys.
+
+    External IDs are necessary for importing data to the products of the server.
+
+    :param db_id_list: list, product.template primary keys
+    :param limit: int, limit the result count
+    :return: pandas dataframe, a list of dicts with ir.model.data rows
     """
     log.info("Fetching product external ids from 'ir.model.data'")
     domain = ["&", ["model", "=", "product.template"],
@@ -69,10 +92,13 @@ def fetch_product_external_id(db_id_list, limit = 0):
 
 
 def fetch_all_dpmc_prices(limit = 0):
-    """
-    All(qty>=0 and qty<0) prices belonging to dpmc pos categories(bajaj, 2w, 3w, qute)
-    :param limit: limit result count
-    :return: dataframe of dicts
+    """Fetch every single Product Prices of DPMC POS Category.
+
+    If available_qty >= 0 or available_qty < 0 retrieve their prices.
+    All products should belonging to DPMC's POS categories (Bajaj, 2W, 3W, QUTE)
+
+    :param limit: int, limit the result count
+    :return: pandas dataframe, a list of dicts with product.template rows containing sales price and cost
     """
     log.info("Fetching product prices from 'product.template'")
     domain = ["&", ["available_in_pos", "=", True],
