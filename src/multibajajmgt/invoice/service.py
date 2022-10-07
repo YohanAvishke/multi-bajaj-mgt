c41a23739e0d4108e81b7b937d6b10c40934c484
YohanAvishke <yaediriweera@gmail.com>
2022-10-07 20:08:31 +0530

    Remove loop and replace with pandas apply

--- src/multibajajmgt/invoice/service.py
+++ src/multibajajmgt/invoice/service.py
@@ -20,51 +20,53 @@ BASE_FILE = f"{DATA_DIR}/invoice/{DocumentResourceType.invoice_dpmc}"
 HISTORY_DIR = f"{DATA_DIR}/invoice/history"
 
 
-def fetch_invoices():
-    invoices = pd.read_json(BASE_FILE, orient = "records").Adjustments.to_list()
-    for invoice in invoices:
-        invoice_type = invoice[JSONField.type]
-        # Could be invoice, order, mobile number
-        default_id = invoice[JSONField.default_id]
-        # For column types used in the request
-        order_field = grn_field = None
-        # Setup payload fields depending on available info in base file
-        if Type.invoice in invoice_type:
-            order_field = grn_field = Field.invoice_no
-        elif Type.order in invoice_type:
-            order_field = Field.dlr_order_no
-            grn_field = Field.order_no
-        elif Type.mobile in invoice_type:
-            order_field = Field.mobile_no
-        # Fetch invoice data(invoice and GRN numbers) from DPMC server
+def _enrich_with_advance_invoice_data(row):
+    invoice_type = row[JSONField.type]
+    # Could be invoice, order, mobile number
+    default_id = row[JSONField.default_id]
+    # For column types used in the request
+    order_field = grn_field = None
+    # Setup payload fields depending on available info in base file
+    if Type.invoice in invoice_type:
+        order_field = grn_field = Field.invoice_no
+    elif Type.order in invoice_type:
+        order_field = Field.dlr_order_no
+        grn_field = Field.order_no
+    elif Type.mobile in invoice_type:
+        order_field = Field.mobile_no
+    # Fetch invoice data(invoice and GRN numbers) from DPMC server
+    try:
+        invoice_data = dpmc_client.inquire_goodreceivenote_by_order_ref(order_field, default_id)
+    except DataNotFoundError:
         try:
-            invoice_data = dpmc_client.inquire_goodreceivenote_by_order_ref(order_field, default_id)
-        except DataNotFoundError:
-            try:
-                # Usually happens when for older invoices since "inquire_goodreceivenote_by_order_ref"'s data expires
-                # fast
-                invoice_data = dpmc_client.inquire_goodreceivenote_by_grn_ref(grn_field, default_id)
-            except DataNotFoundError as e:
-                log.error(f"Invoice retrieval failed for {default_id} of {invoice_type}", e)
-                invoice[JSONField.status] = Status.failed
-                continue
-        if len(invoice_data) > 1:
-            # If "id" is incomplete and matches parts of multiple invoice ids.
-            invoice[JSONField.status] = Status.multiple
-            invoice[JSONField.invoice_id] = [invoice_data[n]["Invoice No"] for n in range(len(invoice_data))]
-        else:
-            invoice_data = invoice_data[0]
-            invoice[JSONField.status] = Status.success
-            invoice[JSONField.invoice_id] = invoice_data["Invoice No"]
-            # Available after a call to "inquire_goodreceivenote_by_grn_ref"
-            if "GRN No" in invoice_data:
-                invoice[JSONField.grn_id] = invoice_data["GRN No"]
-            if "Order No" in invoice_data:
-                invoice[JSONField.order_id] = invoice_data["Order No"]
-        log.info(f"Invoice retrieved success for {default_id} of {invoice_type}")
-        # Remove default data
-        del invoice[JSONField.default_id]
-    # Save fetched data
-    adjustments = {JSONField.invoices: invoices}
+            # Usually happens when for older invoices since "inquire_goodreceivenote_by_order_ref"'s data expires
+            # fast
+            invoice_data = dpmc_client.inquire_goodreceivenote_by_grn_ref(grn_field, default_id)
+        except DataNotFoundError as e:
+            log.error(f"Invoice retrieval failed for {default_id} of {invoice_type}", e)
+            row[JSONField.status] = Status.failed
+            return row
+    if len(invoice_data) > 1:
+        # If "id" is incomplete and matches parts of multiple invoice ids.
+        row[JSONField.status] = Status.multiple
+        row[JSONField.invoice_id] = [invoice_data[n]["Invoice No"] for n in range(len(invoice_data))]
+    else:
+        invoice_data = invoice_data[0]
+        row[JSONField.status] = Status.success
+        row[JSONField.invoice_id] = invoice_data["Invoice No"]
+        # Available after a call to "inquire_goodreceivenote_by_grn_ref"
+        if "GRN No" in invoice_data:
+            row[JSONField.grn_id] = invoice_data["GRN No"]
+        if "Order No" in invoice_data:
+            row[JSONField.order_id] = invoice_data["Order No"]
+    log.info(f"Invoice retrieved success for {default_id} of {invoice_type}")
+    # Remove default data
+    row = row.drop(JSONField.default_id)
+    return row
+
+
+def fetch_invoices():
+    invoice_df = pd.read_json(BASE_FILE, orient = "records", convert_dates = False)
+    invoice_df = invoice_df.apply(_enrich_with_advance_invoice_data, axis = 1)
     historical_file_path = mk_historical(get_curr_dir(HISTORY_DIR), DocumentResourceType.invoice_dpmc)
-    write_to_json(historical_file_path, adjustments)
+    invoice_df.to_json(historical_file_path, orient = "records")
